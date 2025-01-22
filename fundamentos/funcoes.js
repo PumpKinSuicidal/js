fc0959e72ef8b10a535d3aa687cbe302e9f8cbb3
Carlos Eduardo <tadashiestaaqui@gmail.com>
2025-01-22 17:16:38 -0300

    app Flex

--- fundamentos/funcoes.js
+++ fundamentos/funcoes.js
@@ -7,4 +7,26 @@ function hello() {
     console.log("Hello World")
 }
 
+console.log(typeof(hello))
 hello()
+
+// Função atribída
+const hello2 = function () {
+// ou "const hello2 = () => {"
+    console.log("Hello teste")
+}
+
+console.log(typeof(hello2))
+hello2()
+
+// Função literal
+function somarS(num1,num2) {
+    return (console.log(num1 + num2))
+}
+
+somarS(1,2)
+
+const somarT = (num1, num2) => console.log(num1 + num2)
+
+somarT(4,5)
+
---
+++ fundamentos/operadores.js
@@ -0,0 +1,24 @@
+/**
+ * Operadores:
+ */
+
+console.clear()
+console.log("Operadores aritimeticos")
+console.log(" + | Soma")
+console.log(" - | Subtração")
+console.log(" * | Multiplicação")
+console.log(" / | Divisão")
+console.log(" % | Resto")
+console.log("")
+console.log("Operadores aritimeticos de atribuição")
+console.log(" += | Soma e atribuição e variavel")
+console.log(" -= | Subtração e atribuição e variavel")
+console.log(" *= | Multiplicação e atribuição e variavel")
+console.log(" /= | Divisão e atribuição e variavel")
+console.log(" %= | Resto e atribuição e variavel")
+console.log("")
+console.log("Comparadores")
+console.log(" = | Atribição")
+console.log(" == | Igual")
+console.log(" === | Indentico - Mesma string e caractere")
+console.log(" !== | Diferente - String diferente")
